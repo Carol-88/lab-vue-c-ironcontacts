539929cba9378f275b58ee6c7ca0218954581df8
Carol-88 <gralleta@gmail.com>
2024-05-06 15:17:35 +0200

    ✅ iteration 1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+   <table>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="contact in contacts" :key="contact.id">
+        <td><img :src="contact.pictureUrl" alt="Contact Picture" width="50"></td>
+        <td>{{ contact.name }}</td>
+        <td>{{ contact.popularity }}</td>
+      </tr>
+    </tbody>
+ </table>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import {ref} from 'vue';
+import contactsData from './contacts.json'
+
+const contacts = ref(contactsData.slice(0,5))
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+table {
+ width: 100%;
+ border-collapse: collapse;
+}
+
+th, td {
+ border: 1px solid #ddd;
+ padding: 8px;
+}
+
+th {
+ background-color: #f2f2f2;
 }
 </style>

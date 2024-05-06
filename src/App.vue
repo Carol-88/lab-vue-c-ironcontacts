62e32b0c9cf7121f6a6dd1e33bb11c6e80dfc5fa
Carol-88 <gralleta@gmail.com>
2024-05-06 15:46:05 +0200

    ✅ iteration 3

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,46 @@
 <template>
-   <table>
-    <thead>
-      <tr>
-        <th>Picture</th>
-        <th>Name</th>
-        <th>Popularity</th>
-        <th>Won an Oscar</th>
-        <th>Won an Emmy</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="contact in contacts" :key="contact.id">
-        <td><img :src="contact.pictureUrl" alt="Contact Picture" width="50"></td>
-        <td>{{ contact.name }}</td>
-        <td>{{ contact.popularity }}</td>
-        <td>
-          <span v-if="contact.wonOscar">🏆</span>
-        </td>
-        <td>
-          <span v-if="contact.wonEmmy">🏆</span>
-        </td>
-      </tr>
-    </tbody>
- </table>
+ <section>
+    <button @click="addRandomContact">Add random Contact</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="contact in contacts" :key="contact.id">
+          <td><img :src="contact.pictureUrl" alt="Contact Picture" width="50"></td>
+          <td>{{ contact.name }}</td>
+          <td>{{ contact.popularity }}</td>
+          <td>
+            <span v-if="contact.wonOscar">🏆</span>
+          </td>
+          <td>
+            <span v-if="contact.wonEmmy">🏆</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+ </section>
 </template>
 
 <script setup>
-import {ref} from 'vue';
-import contactsData from './contacts.json'
+import { ref } from 'vue';
+import contactsData from './contacts.json';
 
-const contacts = ref(contactsData.slice(0,5))
+const contacts = ref(contactsData.slice(0, 5));
+const randomContacts = ref(contactsData.slice(5));
+
+const addRandomContact = () => {
+  if (randomContacts.value.length > 0) {
+    const randomContact = randomContacts.value.splice(Math.floor(Math.random() * randomContacts.value.length), 1)[0];
+    contacts.value.push(randomContact);
+ }
+};
 </script>
 
 <style>
